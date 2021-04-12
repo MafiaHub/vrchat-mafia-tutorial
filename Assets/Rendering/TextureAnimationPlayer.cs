b700883fb33708c8d45d4dc924ee890ff3201ef1
Dominik Madarász <zaklaus@outlook.com>
2021-04-12 16:20:23 +0200

    stuff

---
+++ Assets/Rendering/TextureAnimationPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using MafiaUnity;
+
+public class TextureAnimationPlayer : MonoBehaviour {
+
+    public List<Texture2D> frames;
+    public Material material;
+    public uint framePeriod;
+
+    private float timeUntilNextFrame;
+    private int frameIndex;
+
+    private void Start()
+    {
+        ResetTime();
+    }
+
+    void Update () {
+        if (timeUntilNextFrame < 0)
+        {
+            ResetTime();
+
+            if (frames.Count > 0)
+                material.SetTexture("_MainTex", frames[frameIndex++ % frames.Count]);
+        }
+        else
+            timeUntilNextFrame -= Time.deltaTime;
+	}
+
+    void ResetTime()
+    {
+        timeUntilNextFrame = framePeriod / 1000f;
+    }
+}
